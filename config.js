a994e4bfd43a4c1b3db1e26e00e2a0961dee4cf1
Michael Nguyen <ninjeezy@gmail.com>
2018-02-03 21:28:25 -0500

    update

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports.tokenAddress = "0xdF9FDFC647605BcD095707537eAda42965EcFea2",
+module.exports.HDWallet_mneumonic = "beyond stamp tube core copper prefer prevent inherit before helmet leave claim",
+module.exports.HDWallet_uri = "https://rinkeby.infura.io/DpRrDJzqd05E5D2ZmTjG",
+module.exports.twilioAccountSid = 'AC93d7f611c3bac6c8452fc406c9b0386e',
+module.exports.twilioAuthToken = 'e493133ac514e3c8ba5265d286dcf4d9',
+module.exports.twilioNumber = '+14708656950',
+module.exports.orgAddress = "0x510c8cCc19faF6A5D4CE366dAc27aA20C96Cb92A"
